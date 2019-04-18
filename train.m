82a85601106d0dc30290fee16b8f7f22fe6ef536
Ozy Vielma <ozviis11@gmail.com>
2019-04-17 22:52:57 -0500

    Add function to remove directories created by dir from the generated training lists.

--- train.m
+++ train.m
@@ -1,9 +1,5 @@
 % This script will perform the entire training sequence
 % Before you run this code, make sure that you are in the right directory 
-% and add the appropriate datapath manually (00_common, 00_common_data, 12_boosting),
-% as hardcoding the datapath will require changing it everytime we
-% pull changes each of us have made.
-% Likewise for all data including training and test images.
 
 % set directory path for code and data files
 clc;
@@ -13,10 +9,12 @@ directories;
 % training faces list
 training_faces_path = [training_directory, 'training_faces'];
 training_faces_list = dir(training_faces_path);
+training_faces_list = remove_directories_from_dir_list(training_faces_list);
 
 % training nonfaces list
 training_nonfaces_path = [training_directory, 'training_nonfaces'];
 training_nonfaces_list = dir(training_nonfaces_path);
+training_nonfaces_list = remove_directories_from_dir_list(training_nonfaces_list);
 
 % get sizes of lists
 num_faces = size(training_faces_list, 1);
@@ -24,7 +22,7 @@ num_nonfaces = size(training_nonfaces_list, 1);
 
 %%
 % First step of the training procedure is to use rectangle filters on all 
-% training images (refer to main_script in 12_boosting code example on TRACS).
+% training images (refer to main_script in 12_boosting code example on TRACS)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -45,5 +43,7 @@ figure(1); imshow(photo, []);
 centroid = (size(photo)/2)/2;
 trainingpatch = imcrop(photo, [centroid 59 59]);
 figure(2); imshow(trainingpatch, []);
+
+
 
 
