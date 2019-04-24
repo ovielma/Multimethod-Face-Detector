6caf3251469db622056f00bc1caa56e8dd1d56a7
Ozy Vielma <ozviis11@gmail.com>
2019-04-23 22:16:43 -0500

    Add classify cascade function and called it in test.m

--- functions/cascade_classify.m
+++ functions/cascade_classify.m
@@ -2,10 +2,20 @@ function result = cascade_classify(window)
 
 % If window is less than a predefined threshold, reject window
 
-for
+threshold = 1;
+
+for i = 1:size(window)
+    if window < threshold
+        window = -1;
+        result = window; 
+        disp('Classifier ', i,  ' was rejected, it is not a face!');
+    elseif window > threshold
+        window = 1;
+        result = window; 
+        disp('Classifier ', i,  ' was classified as a possible face!');
+    end
     
 end
-
 
 end
 
--- test.m
+++ test.m
@@ -1,8 +1,7 @@
+%
 % test.m tests face detector using skin detection and classifier cascades
 % using a classifier trained with AdaBoost and bootstrapping
-
-
-
+%
 %%
 clear;
 directories;
@@ -20,10 +19,16 @@ load boosted50;
 load classifiers5000;
 threshold = 5;
 
+%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%%  Testing Without Skin Detection & Classifier Cascades
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %TESTING FOR NONFACES IMAGES
-%%
-
-
 
 predicted = 0;
 miss = 0;
@@ -50,7 +55,6 @@ end
 
 nonFaceAcc = (predicted/num_testing_nonfaces) * 100;
 
-
 %%
 %TESTING CROPPED FACES IMAGES
 testing_cropped_faces_path = [training_directory, 'test_cropped_faces'];
@@ -59,10 +63,7 @@ testing_cropped_faces_list = remove_directories_from_dir_list(testing_cropped_fa
 
 num_testing_croppedfaces = size(testing_cropped_faces_list, 1);
 
-
-
 %TESTING FOR CROPPED FACES
-%%
 predicted = 0;
 miss = 0;
 
@@ -87,7 +88,6 @@ end
 
 croppedFaceAcc = (predicted/num_testing_croppedfaces) * 100;
 
-
 %%
 %TESTING FACES IMAGES
 testing_faces_path = [training_directory, 'test_face_photos'];
@@ -99,7 +99,6 @@ num_testing_faces = size(testing_faces_list, 1);
 
 
 %TESTING FOR FACES
-%%
 predicted = 0;
 miss = 0;
 total = 0;
@@ -124,7 +123,6 @@ end
 
 FaceAcc = (predicted/20) * 100;
 
-
 %% 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -138,9 +136,7 @@ FaceAcc = (predicted/20) * 100;
 negative_histogram = read_double_image('negatives.bin');
 positive_histogram = read_double_image('positives.bin');
 
-%%
-
-for i =1:1 %num_testing_faces-1
+for i =1:num_testing_faces-1
     test_img1 = getfield(testing_faces_list(i),'name');
     test_img = double(imread(test_img1));
     %imshow(test_img/255);
@@ -150,14 +146,40 @@ for i =1:1 %num_testing_faces-1
     if(size(test_img, 3) == 3)
         result_on_skin = detect_skin(test_img, positive_histogram,  negative_histogram);
         %figure (i); imshow(result_on_skin, []);
-                     
-        % run classifier after skin detection
-        %result = apply_classifier_aux(result_on_skin, boosted_classifier, weak_classifiers, [60 60]);
-        %figure(i); imshow(result, []);
         
         result = boosted_detector_demo(test_img2,test_img2, 2,  boosted_classifier, ...
-                          weak_classifiers, [60,60], 2);
+                          weak_classifiers, [60,60], 4);
         figure(i); imshow(result, []);
     end
       
 end
+
+close all;
+
+%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%%  Classifier Cascades
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+cascade_faces = zeros(:);
+cascade_nonfaces = zeros(:);
+
+%%
+for i = 1:classifier_number-1
+    
+    result = cascade_classify(window)
+    
+    if result == 1
+        cascade_faces = result;
+    elseif result == -1
+        cascade_nonfaces = result;
+    end
+    
+end
+
+
+
